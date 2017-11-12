f7527723bfb70b0fbfdef2a1ba1479a0c60dc07f
jesada_gon <jesada.gon@ascendcorp.com>
2017-11-12 13:29:18 +0700

    add test function step

---
+++ vars/acnTestFunc.groovy
@@ -0,0 +1,27 @@
+#!/usr/bin/groovy
+import io.fabric8.Fabric8Commands
+import io.fabric8.Utils
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def flow = new io.fabric8.Fabric8Commands()
+    def utils = new io.fabric8.Utils()
+
+    def appName = config.APP_NAME
+    def newVersion = config.VERSION
+    dockerTagAndPush(appName,newVersion)
+  } // End Function
+
+def dockerTagAndPush(appname,version){
+    def utils = new Utils()
+    def flow = new Fabric8Commands()
+    def namespace = utils.getNamespace()
+    def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${appname}:${version}"
+
+    sh "echo \"newImageName=${newImageName}\""
+}
