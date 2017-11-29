20acbe9ac3175c6203357359a39a77f18e89c7c7
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-11-29 18:30:14 +0700

    add library for support pull images from docker hub and change image name

---
+++ vars/acnImageBuildFromDockerhub.groovy
@@ -0,0 +1,43 @@
+#!/usr/bin/groovy
+import io.fabric8.Fabric8Commands
+import io.fabric8.Utils
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def flow = new io.fabric8.Fabric8Commands()
+    def utils = new io.fabric8.Utils()
+
+    /*container('clients') {
+        def appName = config.APP_NAME
+        def newVersion = config.VERSION
+        dockerBuild(appName,newVersion)
+    }*/
+    def appName = config.APPNAME
+    def newVersion = config.VERSION
+    def repositoryName = config.REPOSITORYNAME
+    def repositoryTag = config.REPOSITORYTAG
+    dockerBuild(appName, newVersion, repositoryName, repositoryTag)
+  } // End Function
+
+def dockerBuild(appname, version, repositoryName, repositoryTag){
+    def utils = new Utils()
+    def flow = new Fabric8Commands()
+    def namespace = utils.getNamespace()
+    def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${appname}:${version}"
+
+    sh "docker pull ${repositoryName}"
+    
+    sh "docker tag ${repositoryName}:${repositoryTag} ${newImageName}"
+    sh "docker build -t ${newImageName} ."
+
+    if (flow.isSingleNode()) {
+        sh "echo 'Running on a single node, skipping docker push as not needed'"
+    } else {
+        sh "docker push ${newImageName}"
+    }
+}
