4e3d0531571ba6d9ce8faf78b103493a4d513097
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-12-06 16:33:44 +0700

    command docker ps

--- vars/acnRobotExecute.groovy
+++ vars/acnRobotExecute.groovy
@@ -38,9 +38,9 @@ def executeRobot(appName, appVersion){
     def cmdRobot = "robot -L Trace -d /opt/robotframework/tests/${appName}/results /opt/robotframework/tests/${appName}"
     // def cmdEchoResult = "echo $?"
 
-    sh "imagesRBT=$(docker run -dti -v /home/jenkins/workspace/${appName}/robot/${appName}:/opt/robotframework/tests/${appName} ascendcorphub/robot:v1.0.0 /bin/bash -c '${cmdInstallLib}; ${cmdCreateDirectory}; ${cmdDirectory}; ${cmdRobot}') "
+    sh "docker run -dti -v /home/jenkins/workspace/${appName}/robot/${appName}:/opt/robotframework/tests/${appName} ascendcorphub/robot:v1.0.0 /bin/bash -c '${cmdInstallLib}; ${cmdCreateDirectory}; ${cmdDirectory}; ${cmdRobot}' "
 
-    sh "echo 'imagesRBT ${imagesRBT}'"
+    sh "docker ps"
 
     if (flow.isSingleNode()) {
         sh "echo 'Running on a single node, skipping docker push as not needed'"
