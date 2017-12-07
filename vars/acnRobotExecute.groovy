a90f3afd71a7492291cd436be546505d33d59515
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-12-07 17:00:15 +0700

    delete step pull docekr

--- vars/acnRobotExecute.groovy
+++ vars/acnRobotExecute.groovy
@@ -24,26 +24,17 @@ def executeRobot(appName, appVersion){
     def flow = new Fabric8Commands()
     def namespace = utils.getNamespace()
 
-    // sh "cd /home/jenkins/workspace/${appName}"
-    sh "cd /home/jenkins/workspace/demo-peacock"
     sh "echo 'appName ${appName}'"
-    sh "docker pull ascendcorphub/robot:v1.0.0"
     sh "pwd"
     // /home/jenkins/workspace/${appName}
     // /home/jenkins/workspace/${appName}/robot
 
-// docker run -dti -v /Users/kwanchanokskuljarernpon/Documents/LUPUS/source_code/demo-peacock-robot/demo-peacock:/opt/robotframework/tests/demo-peacock ascendcorphub/robot:v1.0.0 /bin/bash -c "pip install -r /opt/robotframework/tests/demo-peacock/requirements.txt; mkdir -p /opt/robotframework/tests/demo-peacock/results; cd /opt/robotframework/tests/demo-peacock/; robot -L Trace -d /opt/robotframework/tests/demo-peacock/results /opt/robotframework/tests/demo-peacock; echo $?"
     def cmdInstallLib = "pip install -r /opt/robotframework/tests/${appName}/requirements.txt"
     def cmdCreateDirectory = "mkdir -p /opt/robotframework/tests/${appName}/results"
     def cmdDirectory = "cd /opt/robotframework/tests/${appName}/"
     def cmdRobot = "robot -L Trace -d /opt/robotframework/tests/${appName}/results /opt/robotframework/tests/${appName}/testcases"
-    // def cmdEchoResult = "echo $?"
-    // /home/jenkins/workspace/peacock-poc/robot/demo-peacock
 
-    // sh "cat /home/jenkins/workspace/${appName}/robot/demo-peacock/testcases/test2.robot"
     sh "cat /home/jenkins/workspace/demo-peacock/robot/demo-peacock/testcases/test2.robot"
-    // sh "cp -r /home/jenkins/workspace/demo-peacock/robot/demo-peacock /home/jenkins/workspace/demo-peacock/robot/demo-peacock"
-    
 
     // sh "docker run -dti --name=${appName}-${appVersion} -v /home/jenkins/workspace/${appName}/robot/demo-peacock:/root/home/opt/robotframework/tests/${appName} ascendcorphub/robot:v1.0.0 /bin/bash -c '${cmdInstallLib}; ${cmdCreateDirectory}; ${cmdDirectory}; ${cmdRobot}'"
     sh "docker run -dti --name=${appName}-${appVersion} -v /home/jenkins/workspace/${appName}/robot/demo-peacock:/root/home/opt/robotframework/tests/${appName} ascendcorphub/robot:v1.0.0 /bin/bash"
