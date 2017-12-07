64b862c509d6e572f2cb9ff9e29441012c7005c2
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-12-07 15:49:12 +0700

    delete  condition

--- vars/acnRobotTemplate.groovy
+++ vars/acnRobotTemplate.groovy
@@ -16,114 +16,39 @@ def call(Map parameters = [:], body) {
     def cloud = flow.getCloudConfig()
 
     // 0.13 introduces a breaking change when defining pod env vars so check version before creating build pod
-    if (utils.isKubernetesPluginVersion013()) {
-        echo "Kubernetes Plugin Version 013"
-        if (utils.isUseOpenShiftS2IForBuilds()) {
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}", serviceAccount: 'jenkins',
-                    containers: [
-                            containerTemplate(
-                                    name: 'jnlp',
-                                    image: "${jnlpImage}",
-                                    args: '${computer.jnlpmac} ${computer.name}',
-                                    workingDir: '/home/jenkins/',
-                                    resourceLimitMemory: '512Mi')],
-                    volumes: [
-                            secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                            persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                            secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                            secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                            secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                            secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git')]) {
+    echo "building using the docker socket"
 
-                body(
-
-                )
-            }
-        } else {
-            echo "building using the docker socket"
-
-            podTemplate(cloud: cloud,
-                    label: label,
-                    inheritFrom: "${inheritFrom}",
-                    containers: [
-                            containerTemplate (
-                                    //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
-                                    name: 'robot',
-                                    image: "${robotImage}",
-                                    command: '/bin/sh -c',
-                                    args: 'cat',
-                                    ttyEnabled: true,
-                                    alwaysPullImage: false,
-                                    workingDir: '/home/jenkins/',
-                                    //resourceLimitMemory: '640Mi',
-                                    envVars: [
-                                            //envVar(key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xms10m -Xmx192m'),
-                                            envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')])],
-                    volumes: [
-                            secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                            secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                            secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                            secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                            secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                            hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
-                            persistentVolumeClaim(mountPath: '/root/data/pvc', claimName: 'nfs-robot', readOnly: false),
-                            nfsVolume(mountPath: '/home/jenkins/workspace', serverAddress: '35.200.192.59', serverPath: '/var/nfsshare', readOnly: false),
-                            hostPathVolume(hostPath: '/home/jenkins/workspace', mountPath: '/home/jenkins/workspace')])
-                    {
-
-                        body(
-
-                        )
-                    }
-        }
-    } else {
-        if (utils.isUseOpenShiftS2IForBuilds()) {
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}", serviceAccount: 'jenkins', restartPolicy: 'OnFailure',
-                    containers: [
-                            [name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins/',
-                             resourceLimitMemory: '512Mi'], // needs to be high to work on OSO
-                            [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
-                             envVars: [
-                                     [key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UseParallelGC -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xmx256m'],
-                                     [key: 'MAVEN_OPTS', value: '-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn']
-                                     ],
-                             resourceLimitMemory: '1024Mi']],
-                    volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                              persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                              secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                              secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                              secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                              secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git')],
-                    envVars: [[key: 'GIT_COMMITTER_EMAIL', value: 'fabric8@googlegroups.com'], [key: 'GIT_COMMITTER_NAME', value: 'fabric8']]) {
-
-                body(
-
-                )
-            }
-        } else {
-            echo "building using the docker socket"
-
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}",
-                    containers: [
+    podTemplate(cloud: cloud,
+            label: label,
+            inheritFrom: "${inheritFrom}",
+            containers: [
+                    containerTemplate (
                             //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
-                            [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
-                             envVars: [
-                                     [key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn']]]],
-                    volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                              persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                              secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                              secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                              secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                              secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                              secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                              hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-                    envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
-            ) {
+                            name: 'robot',
+                            image: "${robotImage}",
+                            command: '/bin/sh -c',
+                            args: 'cat',
+                            ttyEnabled: true,
+                            alwaysPullImage: false,
+                            workingDir: '/home/jenkins/',
+                            //resourceLimitMemory: '640Mi',
+                            envVars: [
+                                    //envVar(key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xms10m -Xmx192m'),
+                                    envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')])],
+            volumes: [
+                    secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                    secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
+                    secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                    secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                    secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                    persistentVolumeClaim(mountPath: '/root/data/pvc', claimName: 'nfs-robot', readOnly: false),
+                    nfsVolume(mountPath: '/home/jenkins/workspace', serverAddress: '35.200.192.59', serverPath: '/var/nfsshare', readOnly: false),
+                    hostPathVolume(hostPath: '/home/jenkins/workspace', mountPath: '/home/jenkins/workspace')])
+            {
 
                 body(
 
                 )
             }
-        }
-    }
 }
