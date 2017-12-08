ed6b9b9c49a571438cb3be0f71aad691fd099eb6
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-12-08 10:48:06 +0700

    delete exception

--- vars/restGetURL.groovy
+++ vars/restGetURL.groovy
@@ -33,10 +33,12 @@ def getResult(url, authString){
     connection.connect()
     rs = new JsonSlurperClassic().parse(new InputStreamReader(connection.getInputStream(),"UTF-8"))
 
-  } catch(e){
-    def mockResult = "{\"build\":{\"version\":\"Cannot get version because service not available\"}}"
-    rs = new JsonSlurperClassic().parseText(mockResult)
-  } finally {
+  } 
+  // catch(e){
+  //   def mockResult = "{\"build\":{\"version\":\"Cannot get version because service not available\"}}"
+  //   rs = new JsonSlurperClassic().parseText(mockResult)
+  // } 
+  finally {
     connection.disconnect()
   }
   echo 'returning'
