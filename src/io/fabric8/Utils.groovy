49d21a2a48e1d40ae46491e64ef48c6feb8a84de
ascendcorp <33472355+ascendcorp@users.noreply.github.com>
2017-12-18 15:39:50 +0700

    Update Utils.groovy

--- src/io/fabric8/Utils.groovy
+++ src/io/fabric8/Utils.groovy
@@ -3,7 +3,7 @@ package io.fabric8
 
 import com.cloudbees.groovy.cps.NonCPS
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
-import io.fabric8.kubernetes.api.model.HasMetadata
+import io.fabric8.kubernetes.client.CustomResource
 import io.fabric8.kubernetes.client.KubernetesClient
 import io.fabric8.openshift.client.DefaultOpenShiftClient
 import io.fabric8.openshift.client.OpenShiftClient
@@ -30,7 +30,7 @@ String environmentNamespace(String environment) {
 
 @NonCPS
 String getApiVersion() {
-  return io.fabric8.kubernetes.api.model.HasMetadata.getApiVersion()
+  return io.fabric8.kubernetes.client.CustomResource.getApiVersion()
 }
 
 /**
