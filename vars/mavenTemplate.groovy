faf497c563ac6a830fffb071b7e245acd5c30ccd
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2017-12-21 16:25:43 +0700

    Revert "change PVC"
    This reverts commit 3f45f3c0459a199c427a8d8efb6364f1bf240e51.

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -137,7 +137,7 @@ def call(Map parameters = [:], body) {
                     ],
                     volumes: [
                         secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                        persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.m2'),
+                        persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
                         secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                         secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                         secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
