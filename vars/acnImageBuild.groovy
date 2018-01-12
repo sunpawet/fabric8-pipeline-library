c371d32d22daae0f659f867d2b56fd5c1e2f6dbb
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2018-01-12 14:18:04 +0700

    modify

--- vars/acnImageBuild.groovy
+++ vars/acnImageBuild.groovy
@@ -14,32 +14,28 @@ def call(body) {
     def appName = config.APPNAME
     def newVersion = config.VERSION
     def countryCode = config.COUNTRYCODE
-    def pathDockerfile = config.PATH_DOCKERFILE ?: "None"
-    // pathDockerfile = /home/jenkins/workspace/fabric8-demo/pipeline/dockerfiles/mountebank
     def pathWorkspaceMockdata = config.PATH_WORKSPACE_MOCKDATA ?: "None"
-    // pathWorkspaceMockdata = home/jenkins/workspace/fabric8-demo/robot/tmp-mockserver/mockdata
-    // PATH_WORKSPACE_MOCKDATA=home/jenkins/workspace/fabric8-demo/robot/tmp-mockserver/mockdata
-    def images = dockerBuild(pathDockerfile, pathWorkspaceMockdata, appName, newVersion, countryCode)
+    // /home/jenkins/workspace/${env.JOB_NAME}/robot/${GIT_INTEGRATION_TEST_NAME}/mockdata
+    // /home/jenkins/workspace/${env.JOB_NAME}/pipeline/dockerfiles/mountebank
+    def images = dockerBuild(pathWorkspaceMockdata, appName, newVersion, countryCode)
     return images
   } // End Function
 
-def dockerBuild(pathDockerfile, pathWorkspaceMockdata, appname, version, countryCode){
+def dockerBuild(pathWorkspaceMockdata, appname, version, countryCode){
     def utils = new Utils()
     def flow = new Fabric8Commands()
     def namespace = utils.getNamespace()
 
     def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${appname}:${version}"
 
-    if ( pathDockerfile == "None" ) {
+    if ( pathWorkspaceMockdata == "None" ) {
         sh "sed -i \"s/#COUNTRY_CODE#/${countryCode}/g\" Dockerfile"
         sh "sed -i \"s/#APP_VERSION#/${version}/g\" Dockerfile"
         sh "docker build --pull -t ${newImageName} ."
         // fabric8-demo:1.0.1-1
     } else {
-        sh "echo pathDockerfile ${pathDockerfile}"
         sh "echo pathWorkspaceMockdata ${pathWorkspaceMockdata}"
-        sh "cd ${pathDockerfile} && sed -i \"s~#PATH_WORKSPACE_MOCKDATA#~${pathWorkspaceMockdata}~g\" Dockerfile"
-        sh "docker build --pull -t ${newImageName} ."
+        sh "cd ${pathWorkspaceMockdata} && docker build --pull -t ${newImageName} ."
         // fabric8-demo-mountebank:1.0.1-1
     }
 
