e881c8ff64b6eaf108b3cc5670cacbc20be04e20
Jesada Gonkratoke <jesada.gon@ascendcorp.com>
2018-03-02 14:45:53 +0700

    Build Image for Fabric8

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -96,6 +96,7 @@ def call(Map parameters = [:], body) {
                              resourceLimitMemory: '1024Mi']],
                     volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
                               persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
+                              secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                               secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                               secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                               secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
